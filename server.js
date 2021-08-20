0ea753c4489d8d10f761aa50ebdd6d41abb4265f
Malek-Ghorbel <malekghorbel@insat.u-carthage.tn>
2021-08-20 15:55:19 +0100

    a

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const bcrypt = require('bcrypt-nodejs') ;
 const db = knex({
     client: 'pg',
     connection : {
-        connectionString: 'postgres://vtyiucabfwiddd:00a791e86b1df2c2065e567352f13eec42995599e014fe2375b7f61d391effd5@ec2-18-214-238-28.compute-1.amazonaws.com:5432/dfkrd7qnllcm2a',
-        ssl: true
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 }) ;
 
